c4aa4b94108d5f2c070e64f5b71cb3ec9e42c401
DSIW <dsiw@dsiw-it.de>
2016-06-09 23:27:38 +0200

    Restyle payment entry

--- app/src/main/java/li/klass/bezahlscanner/PaymentAdapter.java
+++ app/src/main/java/li/klass/bezahlscanner/PaymentAdapter.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
 import java.util.List;
 
 public class PaymentAdapter extends ArrayAdapter<Payment> {
@@ -30,16 +31,12 @@ public class PaymentAdapter extends ArrayAdapter<Payment> {
         Payment item = getItem(position);
         setText(convertView, R.id.date, item.getDate().toString("YYYY-MM-dd HH:mm:ss"));
         setText(convertView, R.id.name, item.getName());
-        setText(convertView, R.id.iban, item.getIban());
-        setText(convertView, R.id.bic, item.getBic());
-        setText(convertView, R.id.amount, item.getAmount());
+        setText(convertView, R.id.iban, formatIBAN(item.getIban()));
+        setText(convertView, R.id.bic, formatBIC(item.getBic()));
+        setText(convertView, R.id.amount, formatAmount(item.getAmount()));
         setText(convertView, R.id.reason, item.getReason());
 
         return convertView;
-    }
-
-    private void setText(View convertView, int id, String value) {
-        ((TextView) convertView.findViewById(id)).setText(value);
     }
 
     public void setData(List<Payment> payments) {
@@ -48,5 +45,31 @@ public class PaymentAdapter extends ArrayAdapter<Payment> {
             insert(payment, 0);
         }
         notifyDataSetChanged();
+    }
+
+    private String formatIBAN(String iban) {
+        return formatAsPackages(iban, 4, " ");
+    }
+
+    private String formatBIC(String bic) {
+        return formatAsPackages(bic, 4, " ");
+    }
+
+    private String formatAsPackages(String value, int packageSize, String packageSeparator) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= value.length(); i++) {
+            sb.append(value.charAt(i-1));
+            if (i % packageSize == 0) sb.append(packageSeparator);
+        }
+        return sb.toString();
+    }
+
+    private String formatAmount(String amount) {
+        if (amount == null || amount.isEmpty()) amount = "0.00";
+        return NumberFormat.getCurrencyInstance().format(Double.parseDouble(amount));
+    }
+
+    private void setText(View convertView, int id, String value) {
+        ((TextView) convertView.findViewById(id)).setText(value);
     }
 }
