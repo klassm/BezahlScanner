3a1b9cdefcae86468b0d004e66625c9a451d7e55
Matthias Klass <matthias.klass@gmail.com>
2016-07-03 11:15:56 +0200

    Make unit tests work

---
+++ app/src/test/java/li/klass/bezahlscanner/BlaTest.java
@@ -0,0 +1,12 @@
+package li.klass.bezahlscanner;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BlaTest {
+    @Test
+    public void should_Name() {
+        assertThat(true).isEqualTo(true);
+    }
+}
